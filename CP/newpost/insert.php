cfb3136265fad596c5ebc806087c669446d42c4a
qfilex <qfilex@gmail.com>
2017-12-05 03:30:00 +0200

    Database connection details change
    Database connection details change

--- CP/newpost/insert.php
+++ CP/newpost/insert.php
@@ -2,7 +2,7 @@
 /* Attempt MySQL server connection. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 try{
-    $pdo = new PDO("mysql:host=localhost;dbname=utilizatori", "root", "");
+    $pdo = new PDO("mysql:host=localhost;dbname=utilizatori", "admin", "1");
     // Set the PDO error mode to exception
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e){
